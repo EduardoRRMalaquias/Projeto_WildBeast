d463fbb20794078e429fd6e2691a00278d7d1b69
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-07-19 16:13:48 -0300

    tooltip

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,31 +1,46 @@
-export default function initTooltip() {}
+export default function initTooltip() {
+  const tooltips = document.querySelectorAll("[data-tooltip]");
 
-const tooltips = document.querySelectorAll("[data-tooltip]");
+  if (tooltips) {
+    tooltips.forEach((item) => {
+      item.addEventListener("mouseover", onMouseOver);
+    });
 
-tooltips.forEach((item) => {
-  item.addEventListener("mouseover", onMouseOver);
-});
+    function onMouseOver(event) {
+      const tooltipBox = criarTooltipBox(this);
+      tooltipBox.style.top = event.pageY + "px";
+      tooltipBox.style.left = event.pageX + "px";
 
-function onMouseOver(event) {
-  const tooltipBox = criarTooltipBox(this);
-  tooltipBox.style.top = event.pageY + "px";
-  tooltipBox.style.left = event.pageX + "px";
+      onMouseMouve.tooltipBox = tooltipBox;
+      this.addEventListener("mousemove", onMouseMouve);
 
-  this.addEventListener("mouseleve", onMouseLeave);
-}
+      onMouseLeave.tooltipBox = tooltipBox;
+      onMouseLeave.element = this;
+      this.addEventListener("mouseleave", onMouseLeave);
+    }
 
-const onMouseLeave = {
-  tooltipBox: '',
-  hendleEvent() {
-    this.tooltipBox.remove();
-  },
-};
+    const onMouseMouve = {
+      handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 20 + "px";
+        this.tooltipBox.style.left = event.pageX + 20 + "px";
+      },
+    };
 
-function criarTooltipBox(element) {
-  const tooltipBox = document.createElement("div");
-  const text = element.getAttribute("aria-label");
-  tooltipBox.classList.add("tooltip");
-  tooltipBox.innerText = text;
-  document.body.appendChild(tooltipBox);
-  return tooltipBox;
+    const onMouseLeave = {
+      handleEvent() {
+        this.tooltipBox.remove();
+        this.element.removeEventListener("mouseleave", onMouseLeave);
+        this.element.removeEventListener("mousemove", onMouseMouve);
+      },
+    };
+
+    function criarTooltipBox(element) {
+      const tooltipBox = document.createElement("div");
+      const text = element.getAttribute("aria-label");
+      tooltipBox.classList.add("tooltip");
+      tooltipBox.innerText = text;
+      document.body.appendChild(tooltipBox);
+      return tooltipBox;
+    }
+  }
 }
