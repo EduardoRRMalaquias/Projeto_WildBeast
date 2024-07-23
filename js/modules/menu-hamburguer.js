252c4c26bcf784cf0d9ac00f4cac017252eec053
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-07-23 10:57:41 -0300

    menu hamburguer

--- js/modules/menu-hamburguer.js
+++ js/modules/menu-hamburguer.js
@@ -1,3 +1,22 @@
-export default function initMenuHamburguer(){
+import clickOutside from "./click-outside.js";
 
+export default function initMenuHamburguer() {
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuLists = document.querySelector('[data-menu="list"]');
+
+  if (menuButton) {
+    const eventos = ["touchstart", "click"];
+
+    function openMenu(event) {
+      menuLists.classList.toggle("ativo");
+      menuButton.classList.toggle("ativo");
+      clickOutside(menuLists, eventos, () => {
+        console.log("teste");
+        menuLists.classList.remove("ativo");
+        menuButton.classList.remove("ativo");
+      });
+    }
+
+    eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+  }
 }
