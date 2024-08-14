86fff61e0b7edfc8abb52840b350b6e6f91e2d0f
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-08-14 16:40:48 -0300

    refatorar refatoracao tabNav class

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,20 +1,31 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.class = "ativo";
+  }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
-
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      const direcao = tabContent[index].dataset.anime;
-      tabContent[index].classList.add("ativo", direcao);
-    }
-
-    tabMenu.forEach((itemTab, index) => {
-      itemTab.addEventListener("click", () => activeTab(index));
+  // artivar tabs de acordo com o index
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.class);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.class, direcao);
+  }
+
+  // adicionar eventos em cas iten da tab nav
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemTab, index) => {
+      itemTab.addEventListener("click", () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      //ativar primeiro item da tabNav
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
