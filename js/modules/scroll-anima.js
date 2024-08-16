85d5150a6b19a578735addf675de222030ced529
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-08-16 14:39:58 -0300

    refazendo scroll-anima

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,23 +3,42 @@ export default class ScrolAnime {
     this.sections = document.querySelectorAll(sections);
     this.mediaScroll = window.innerWidth * 0.6;
 
-    this.animaScrol = this.animaScrol.bind(this);
+    this.checkDistances = this.checkDistances.bind(this);
   }
 
-  animaScrol() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisivel = sectionTop - this.mediaScroll < 0;
-      if (sectionVisivel) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // pega a distancia de cada item em rela~ção ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.mediaScroll),
+      };
+    });
+  }
+
+  // verifica a distanca em cada objeto em relação a cada scroll do site
+  checkDistances() {
+    this.distance.forEach((section) => {
+      if (window.scrollY > section.offset) {
+        section.element.classList.add("ativo");
+      } else if (section.element.classList.contains("ativo")) {
+        section.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
-    this.animaScrol();
-    window.addEventListener("scroll", this.animaScrol);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistances();
+      window.addEventListener("scroll", this.checkDistances);
+    }
+    return this;
+  }
+
+  // remove os eventos o que faz parar a funcionalidade
+  stop() {
+    window.removeEventListener("scroll", this.checkDistances);
   }
 }
