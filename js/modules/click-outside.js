2b880ac9e5aaabf81d3ba23b59980c94934757b9
Eduardo Rodrigues R Malaquias <malaquias.eduardo18@gmail.com>
2024-08-19 16:36:11 -0300

    corrigindo clççick outside

--- js/modules/click-outside.js
+++ js/modules/click-outside.js
@@ -2,7 +2,7 @@ export default function clickOutside(element, events, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
 
-  // adiciona os eventos passados ao html e adiciona o atributo no element caso ele não tenha
+  // adiciona os eventos passados ao html e adiciona o atributo no element caso ele não tenha os evento
   if (!element.hasAttribute(outside)) {
     events.forEach((userEvent) => {
       setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
@@ -10,9 +10,10 @@ export default function clickOutside(element, events, callback) {
     element.setAttribute(outside, "");
   }
 
-  // remove o evento do html e exedcuta a funcao passada caso o usuario clique fora
+  // remove o evento e o atributo do html e executa a funcao passada caso o usuario clique fora
   function handleOutsideClick(event) {
     if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
       events.forEach((userEvent) => {
         html.removeEventListener(userEvent, handleOutsideClick);
       });
